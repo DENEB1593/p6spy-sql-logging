1faa8d6a0c37c2b722b8c24df57507b8615d50cc
deneb1593 <deneb1593@gmail.com>
2023-01-30 12:27:06 +0900

    설정 등록 단위테스트 추가

---
+++ src/test/java/io/study/deneb/configure/P6SpyConfigureTest.java
@@ -0,0 +1,35 @@
+package io.study.deneb.configure;
+
+import com.p6spy.engine.spy.P6SpyDriver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class P6SpyConfigureTest {
+
+  @Autowired
+  ApplicationContext context;
+
+  @Autowired
+  DataSource dataSource;
+
+  @Test
+  void DataSourceLoaded() {
+    Assertions.assertNotNull(dataSource);
+  }
+
+  @Test
+  void P6SpyConfigureLoaded() {
+    P6SpySqlFormatConfigure p6SpySqlFormatConfigure = context.getBean(P6SpySqlFormatConfigure.class);
+    assertNotNull(p6SpySqlFormatConfigure);
+
+  }
+
+}
